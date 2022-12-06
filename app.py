537598a985c9be286f8097f1640471c20741bf4f
lchaconc <luis.chacon.campos@gmail.com>
2022-12-06 13:17:22 -0600

    Revert "imp de websockets"
    This reverts commit b11d7cd33b74f76b48f6f15be2f7e6076dde83d5.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
-import time 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send
 from src.controller import *
 
 
@@ -12,10 +10,6 @@ UPLOAD_FOLDER= os.getenv('UPLOAD_FOLDER')
 DATA_STUDENTS= os.getenv('DATA_STUDENTS')
 app.config['UPLOAD_FOLDER']= UPLOAD_FOLDER
 app.config['DATA_STUDENTS']= DATA_STUDENTS
-
-app.config['SECRET_KEY'] = "secreta"
-socketio = SocketIO(app)
-
 enrollment=""
 
 
@@ -23,31 +17,15 @@ enrollment=""
 def home ():
     global enrollment
     enrollment = loadEnrollment (app.config['DATA_STUDENTS'])   
-    #print ("students", enrollment)
+    print ("students", enrollment)
     return render_template(  "home.html")
-
-@socketio.on('message')
-def handleMEssage(msg):
-    print ("Message", msg)
-    resend()
-
-def resend ():
-    send("hola desde el server")
-    time.sleep(2)
-    send ("Hola de nuevo")
-    time.sleep(2)
-    send ("Nos vemos...")
-    
 
 
 @app.route('/validator', methods = ['POST'])
 def upload_file():
-    socketio.emit("test")  
-    emails = read_file(request, 'file')
-    print("emails", emails)
-    socketio.emit("ddd") 
-    res = validate_emails(emails, enrollment)     
-    
+    emails = read_file(request, 'file')    
+    res = validate_emails(emails, enrollment)
+
     if res:
         return render_template(  "results.html", wrong_emails=res )
     return render_template(  "all-ggod.html" )
