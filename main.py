b11d7cd33b74f76b48f6f15be2f7e6076dde83d5
lchaconc <luis.chacon.campos@gmail.com>
2022-12-06 13:11:47 -0600

    imp de websockets -cabio en rutas -imp de js en cliente -cambio de input rn cliente

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import time 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
 from src.controller import *
 
 
@@ -10,6 +12,10 @@ UPLOAD_FOLDER= os.getenv('UPLOAD_FOLDER')
 DATA_STUDENTS= os.getenv('DATA_STUDENTS')
 app.config['UPLOAD_FOLDER']= UPLOAD_FOLDER
 app.config['DATA_STUDENTS']= DATA_STUDENTS
+
+app.config['SECRET_KEY'] = "secreta"
+socketio = SocketIO(app)
+
 enrollment=""
 
 
@@ -17,15 +23,31 @@ enrollment=""
 def home ():
     global enrollment
     enrollment = loadEnrollment (app.config['DATA_STUDENTS'])   
-    print ("students", enrollment)
+    #print ("students", enrollment)
     return render_template(  "home.html")
+
+@socketio.on('message')
+def handleMEssage(msg):
+    print ("Message", msg)
+    resend()
+
+def resend ():
+    send("hola desde el server")
+    time.sleep(2)
+    send ("Hola de nuevo")
+    time.sleep(2)
+    send ("Nos vemos...")
+    
 
 
 @app.route('/validator', methods = ['POST'])
 def upload_file():
-    emails = read_file(request, 'file')    
-    res = validate_emails(emails, enrollment)
-
+    socketio.emit("test")  
+    emails = read_file(request, 'file')
+    print("emails", emails)
+    socketio.emit("ddd") 
+    res = validate_emails(emails, enrollment)     
+    
     if res:
         return render_template(  "results.html", wrong_emails=res )
     return render_template(  "all-ggod.html" )
